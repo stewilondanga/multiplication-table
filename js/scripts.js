2aefc9eb1ab2db3819bed4fea976d45b5e67f192
Stewart ilondanga <stilondanga@gmail.com>
2020-09-16 15:28:06 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,33 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+var yxy = 12;
+var redo = function(val) {
+  yxy = val;
+  document.querySelector('table').innerHTML = '<tr id="top-row"></tr>';
+  document.querySelector('#top-row').innerHTML = '';
+  appendToTopRow('<th>&times;</th>');
+  for (var y = 1; y <= yxy; y++) {
+    var arr = [('<th class="downth">' + y + '</th>')];
+    appendToTopRow('<th>' + y + '</th>');
+    for (var x = 1; x <= yxy; x++) {
+      arr.push('<td>' + (x * y) + '</td>');
+    }
+    appendRow(arr);
+  }
+};
+window.onload = function() {
+  document.querySelector('#yxy').value = yxy;
+  redo(yxy);
+}
+function appendToTopRow(str) {
+  document.querySelector('#top-row').innerHTML += '    ' + str + "\n";
+}
+function appendRow(tds) {
+  var content = '';
+  for (var y = 0; y < tds.length; y++) {
+    content += tds[y] + "\n";
+  }
+  document.querySelector('#table').innerHTML += content;
+}
